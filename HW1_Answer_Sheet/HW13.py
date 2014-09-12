001b7d7057bdd08d2079c46b442d569b28262504
Yifeng Ding <yding@tamu.edu>
2014-09-12 09:36:17 -0500

    add datetime object

--- HW1_Answer_Sheet/HW13.py
+++ HW1_Answer_Sheet/HW13.py
@@ -1,13 +1,26 @@
+import datetime
+
 def file_reader(filename):
   f = open(filename)
   result=list()
   date=list()
+  dates=list()
   disch=list()
   for line in f.readlines():
-    if not len(line) or line.startswith('#'):   
-      continue           
-    result.append(line) 
+      if not len(line) or line.startswith('#'):   
+	        continue           
+      result.append(line) 
   for line in result:
-    date.append(line.split()[2])
-    disch.append(line.split()[3])
-  return date,disch
+      date.append(line.split()[2])
+      disch.append(line.split()[3])
+   
+  for line in date:
+      time=line.split('-')
+      year=int(time[0])
+      month=int(time[1])
+      day=int(time[2])
+
+      times=datetime.date(year, month, day)
+
+      dates.append(times)
+  return dates,disch
