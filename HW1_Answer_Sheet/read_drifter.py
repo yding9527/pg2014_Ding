db47b356be7bf83da6bb5faa0dc527ba93e2b71e
Yifeng Ding <yding@tamu.edu>
2014-09-16 16:10:00 -0500

    final draft

--- HW1_Answer_Sheet/read_drifter.py
+++ HW1_Answer_Sheet/read_drifter.py
@@ -4,6 +4,7 @@ def read_drifter(fname):
 	f=open(fname)
 
 	names=[]
+	dic={}
 
 	for line in f.readlines():
 		data=line.split()
@@ -11,58 +12,29 @@ def read_drifter(fname):
 			names.append(data[1])
 
 	for name in names:
-		ff=open(fname)
-		print name,1
-        while 1:
-			line=ff.readline()
+		f=open(fname)
+		coord=[]
+		while 1:
+			line=f.readline()
 			lines=line.split()
-			print name,2
-			time.sleep(1)
 			if len(lines) == 7  and lines[1]==name and lines[0] == 'Track' :
-				print lines[1], lines[0], len(lines)
-				print name
-				time.sleep(1)
-
-#				break
-#				f.readline()
-#				f.readline()
-#				f.readline()
-#				line=f.readline()
-#				print line
-#				lines=line.split()
-
-
-
-
-#for name in names:
-#	for line in ff.readlines():
-#		data=line.split()
-#		if not len(data)==0 and data[0] == 'Track' and data[1] == name:
-#			line2=ff.readline()
-#			print line2
-#				data=line2.split()
-#				print data[0]
-#			line2=ff.readline()
-#			print line2
-#				data=line2.split()
-#				print data[0]
-#			line2=ff.readline()
-#			print line2
-#				data=line2.split()
-#				print data[0]
-#					print line2
-#					print type(line2)
-#					print 2
-
-#					data2=line2.split()
-#					points=[]
-#					if not len(data)==0 and data[0] == 'Trackpoint':
-#						print line2
-#						lat=float(data[1][1:3])+(float(data[2])%100.0)/60.0
-#						lon=float(data[3][1:3])+(float(data[4])%100.0)/60.0
-                        
-#						points=points.append((lon,lat))
-#						print points                          
+				f.readline()
+				f.readline()
+				f.readline()
+				while 1:
+					line=f.readline()
+					lines=line.split()
+					if not len(lines) == 0:
+						lat=float(lines[1][1:3])+(float(lines[2])%100.0)/60.0
+						lon=float(lines[3][1:3])+(float(lines[4])%100.0)/60.0
 						
-                      
-            
+						coord.append((float(int(lat*10000))/10000,float(int(lon*10000))/10000)) 
+
+					else: 
+						break
+					dic[name]=coord
+				break
+			if not line:
+				break
+           
+	return dic
