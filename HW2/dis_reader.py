0bdd13ba49df9adfbe65296a3ca5b91e4f72c601
Yifeng Ding <yding@tamu.edu>
2014-10-09 11:25:11 -0500

    HW2.4

--- HW2/dis_reader.py
+++ HW2/dis_reader.py
@@ -1,4 +1,12 @@
+# Yifeng Ding
+# 2014-10-06
+#
+#Create a class to read discharge data
+#[date, discharge] = data_inquire(self, year)
+#
+
 import datetime
+import matplotlib.pyplot as plt
 
 class Discharge(object):
 
@@ -18,7 +26,7 @@ class Discharge(object):
     for line in result:
         if line.split()[0] == 'USGS' and len(line.split()) >3 :  
             date.append(line.split()[2])
-            disch.append(line.split()[3])
+            disch.append(float(line.split()[3])*0.0283168)
     
     for line in date:
         time=line.split('-')
@@ -33,6 +41,7 @@ class Discharge(object):
 
     return dates, disch
 
+
   def data_inquire(self,year=None):
       dates, disch = self.reader()    
       self.dates = list()
@@ -45,4 +54,9 @@ class Discharge(object):
                   self.dates.append(yr)
                   self.disch.append(disch[dates.index(yr)])
 
+      fig = plt.figure()
+      plt.plot(self.dates, self.disch)
+      fig.suptitle('Hydrograph Over %d'%year, fontsize=20)
+      plt.xlabel('Time Series', fontsize=16)
+      plt.ylabel('Discharge($m^3/s$)', fontsize=16)
       return self.dates, self.disch
