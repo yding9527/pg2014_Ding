818472eca4bf8b3f37724ac441eaf386f04760cd
Yifeng Ding <yding@tamu.edu>
2014-10-13 23:55:27 -0500

    revised hw2.3

--- HW2/high_pass.py
+++ HW2/high_pass.py
@@ -11,13 +11,5 @@ def high_pass(X,Y,rank=1):
 	psol = np.polynomial.Polynomial.fit(X, Y, rank)
 
 	diff = Y-psol(X)
-	order_pos = np.where(diff > 0)
-	order_neg = np.where(diff < 0)
 
-	diff_pos = np.zeros(diff.shape)
-	diff_neg = np.zeros(diff.shape)
-
-	diff_pos[order_pos]=diff[order_pos]
-	diff_neg[order_neg]=diff[order_neg]
-
-	return diff_neg,diff_pos
+	return diff
