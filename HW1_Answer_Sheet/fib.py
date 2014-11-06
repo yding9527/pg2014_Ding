db73868271eb9f2c74a90abedc85e87a24d64bf9
yding9527 <yding@tamu.edu>
2014-11-06 13:08:11 -0600

    Update fib.py
    Revised. Annotation added.

--- HW1_Answer_Sheet/fib.py
+++ HW1_Answer_Sheet/fib.py
@@ -1,6 +1,15 @@
+#Yifeng Ding
 #Fibonacci number module
 
 def fib(n):
+  """Function to return a series of N Fibonacci numbers
+  
+  Inputs: n
+  number of Fibonacci numbers will be returned, range [1,Inf)
+  
+  Outputs: 
+  a list of N Fibonacci numbers, starting with 1"""
+  
   result=[]
   if n==1:
     result=[1,]
