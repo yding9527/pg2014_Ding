44eafbaa494fd4195dad928434bd529b459fd4ca
yding9527 <yding@tamu.edu>
2014-11-06 13:15:32 -0600

    Update trapezoidal.py
    Revised. Annotation added.

--- HW1_Answer_Sheet/trapezoidal.py
+++ HW1_Answer_Sheet/trapezoidal.py
@@ -1,4 +1,16 @@
+#Yifeng Ding
+#Trapezoidal integration
+
 def integrate(f,dx=1):
+  """Function can be used to compute the integral of a list of numbers.
+  
+  inputs:
+  f:number list for integration
+  dx:integration step, default set to be 1
+  
+  outputs:
+  integral result of the input list, calculated with trapezoidal rule."""
+  
   N=len(f)
   result=0.0
   for i in range(0,N-1):
