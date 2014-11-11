30bf8d09f22ed3a7befebc9bb105858323e1a365
Yifeng Ding <yding@tamu.edu>
2014-11-11 11:40:02 -0600

    class properties added; initial states varies

--- HW3/f_shallow_wat.py
+++ HW3/f_shallow_wat.py
@@ -2,39 +2,51 @@
 # Shallow Water Model
 # Yifeng Ding
 
-def shallow_wat(n_time,n_pts,H,dx,dt):
-	import numpy as np
-	import matplotlib.pyplot as plt
-	import time
-	import pylab
 
-	g=9.8
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import pylab
 
-	t=np.array(range(0,n_time))
-	x=np.array(range(0,n_pts))
-	eta=np.array(np.zeros([n_pts,n_time]))
-	u  =np.array(np.zeros([n_pts+1,n_time]))
-	eta[n_pts/2.,0]=H/2.
+class shallow_wat(object):
 
+	def __init__(self,n_time,n_pts,H,dx,dt):
+		self.nt=n_time
+		self.np=n_pts
+		self.H =H
+		self.dx=dx
+		self.dt=dt
 
-	T,X=np.meshgrid(t,x)
-	print eta[:,0]
-
-	for i in range(1,n_time):
-		u[1:-1,i] = u[1:-1,i] - g*dt/dx*(eta[1:,i-1]-eta[:-1,i-1])
-		eta[:,i] = eta[:,i] - H*dt/dx*(u[1:,i]-u[:-1,i])
-		print i
-		print u[:,i]
-	#	print eta[:,i]
-	#	time.sleep(2.5)
-
-	for nn in range(n_time):
-		plt.clf();plt.plot(eta[:,nn]);
-		pylab.ylim([-6,6])
-		plt.savefig('./frames/frame_%04d.png' % nn)
+	def eta(self):
+		g=9.8
+		t=np.array(range(0,self.nt))
+		x=np.array(range(0,self.np))
+		eta=np.array(np.zeros([self.np,self.nt]))
+		u  =np.array(np.zeros([self.np+1,self.nt]))
+#		eta[self.np/2.,0]=self.H/2.
+		eta[0,0]=self.H/2.
 
 
-	return X,T,eta
-  
+		T,X=np.meshgrid(t,x)
+
+		for i in range(1,self.nt):
+			u[1:-1,i] = u[1:-1,i] - g*self.dt/self.dx*(eta[1:,i-1]-eta[:-1,i-1])
+			eta[:,i] = eta[:,i] - self.H*self.dt/self.dx*(u[1:,i]-u[:-1,i])
+
+		for nn in range(self.nt):
+			plt.clf();plt.plot(eta[:,nn]);
+			pylab.ylim([-6,6])
+			plt.savefig('./frames/frame_%04d.png' % nn)
+
+		self.eta=eta
+		self.X  =X
+		self.T  =T
+
 import matplotlib.pyplot as plt  
-X,T,eta=shallow_wat(n_time=100,n_pts=21,H=10.,dx=100.,dt=5.0)
+if __name__ == '__main__':
+	data=shallow_wat(n_time=100,n_pts=21,H=10.,dx=100.,dt=5.0)
+	data.eta()
+
+
+
+
